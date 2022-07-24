77c3de4c9ce807e40ca3e71be8ffe6f1caf19376
AlphaKeks <contact@schnose.eu>
2022-07-24 16:01:03 +0200

    1.3.0: added "bing?"

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import "dotenv/config";
+
+module.exports = {
+	name: "messageCreate",
+
+	execute(msg: Message) {
+		if (msg.author.id === process.env.BOT_ID) return;
+
+		if (msg.content.toLowerCase().includes("bing?")) {
+			if (msg.author.id === "291585142164815873")
+				return msg.reply({ content: "chilling 🍦" });
+			else
+				return msg.reply({
+					content: `${Math.round(Math.random()) ? "chilling 🍦" : "no 😔"}`,
+				});
+		}
+	},
+};
