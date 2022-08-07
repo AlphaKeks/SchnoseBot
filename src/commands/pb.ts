49197816539034994f11b859694ebce682a56708
AlphaKeks <contact@schnose.eu>
2022-08-07 18:29:16 +0200

    1.15.1: WIP /pb

---
+++ src/commands/pb.ts
@@ -0,0 +1,120 @@
+import {
+	SlashCommandBuilder,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+} from "discord.js";
+import { reply } from "../lib/functions/discord";
+import { parseTime } from "../lib/functions/util";
+import { validateTarget } from "../lib/functions/schnose";
+import { getMaps, getPB, validateMap } from "gokz.js";
+import userSchema from "../lib/schemas/user";
+import "dotenv/config";
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("pb")
+		.setDescription("Check a player's personal best on a map.")
+		.addStringOption((o) =>
+			o.setName("mapname").setDescription("Specify a map.").setRequired(true)
+		)
+		.addStringOption((o) =>
+			o.setName("mode").setDescription("Specify a mode.").setChoices(
+				{
+					name: "KZT",
+					value: "kz_timer",
+				},
+				{
+					name: "SKZ",
+					value: "kz_simple",
+				},
+				{
+					name: "VNL",
+					value: "kz_vanilla",
+				}
+			)
+		)
+		.addStringOption((o) =>
+			o.setName("target").setDescription("Specify a target.")
+		),
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		interaction.deferReply();
+
+		const inputMap = interaction.options.getString("mapname")!;
+		const inputMode = interaction.options.getString("mode") || null;
+		const inputTarget = interaction.options.getString("target") || null;
+
+		const globalMaps = await getMaps();
+		if (!globalMaps.success)
+			return reply(interaction, { content: globalMaps.error });
+
+		const mapValidation = await validateMap(inputMap, globalMaps.data);
+		if (!mapValidation.success)
+			return reply(interaction, { content: mapValidation.error });
+
+		let mode: string;
+		if (inputMode) mode = inputMode;
+		else {
+			const userDB = await userSchema.find({ discordID: interaction.user.id });
+			if (!userDB[0]?.mode)
+				return reply(interaction, {
+					content:
+						"You must either specify a mode or set a default value using `/mode`.",
+				});
+			else mode = userDB[0].mode;
+		}
+
+		const targetValidation = await validateTarget(interaction, inputTarget);
+		if (!targetValidation.success)
+			return reply(interaction, { content: targetValidation.error });
+
+		const req = await Promise.all([
+			await getPB(
+				targetValidation.data.value,
+				mapValidation.data.name,
+				0,
+				mode,
+				true
+			),
+			await getPB(
+				targetValidation.data.value,
+				mapValidation.data.name,
+				0,
+				mode,
+				false
+			),
+		]);
+
+		const embed = new EmbedBuilder()
+			.setColor([116, 128, 194])
+			.setTitle(`[WR] - ${mapValidation.data.name}`)
+			.setURL(`https://kzgo.eu/maps/${mapValidation.data.name}`)
+			.setThumbnail(
+				`https://raw.githubusercontent.com/KZGlobalTeam/map-images/master/images/${mapValidation.data.name}.jpg`
+			)
+			.addFields([
+				{
+					name: "TP",
+					value: `${parseTime(req[0].data?.time || 0)} (${
+						req[0].data?.player_name || "-"
+					})`,
+					inline: true,
+				},
+				{
+					name: "PRO",
+					value: `${parseTime(req[1].data?.time || 0)} (${
+						req[1].data?.player_name || "-"
+					})`,
+					inline: true,
+				},
+			])
+			.setFooter({
+				text: "(͡ ͡° ͜ つ ͡͡°)7",
+				iconURL: process.env.ICON,
+			});
+
+		return reply(interaction, {
+			embeds: [embed],
+		});
+	},
+};
---
+++ src/lib/functions/schnose.ts
@@ -0,0 +1,63 @@
+import { Interaction, UserMention } from "discord.js";
+import { getPlayer } from "gokz.js";
+import userSchema from "../schemas/user";
+
+export async function validateTarget(
+	interaction: Interaction,
+	input: null | string | UserMention
+): Promise<{ success: boolean; data?: any; error?: string }> {
+	const res: { success: boolean; data?: any; error?: string } = {
+		success: false,
+		data: { type: "name" },
+	};
+
+	if (!input) {
+		const userDB = await userSchema.find({ discordID: interaction.user.id });
+		if (!userDB[0]?.steamID) res.data = { type: null };
+		else
+			res.data = {
+				type: "steamID",
+				value: userDB[0].steamID,
+			};
+	} else {
+		if (input.startsWith("<@") && input.endsWith(">"))
+			res.data = { type: "mention" };
+		else if (/STEAM_[0-1]:[0-1]:[0-9]+/.test(input))
+			res.data = { type: "steamID" };
+		else res.data = { type: "name" };
+	}
+
+	switch (res.data.type) {
+		case null:
+			res.error =
+				"You did not specify a target and you also haven't saved a steamID in the database. You can do that with `/setsteam`, so you don't have to specify a target everytime you use a command.";
+			break;
+		case "mention": {
+			input = input!.slice(2, -1);
+			if (input.startsWith("!")) input = input.slice(1);
+			const userDB = await userSchema.find({ discordID: input });
+			if (!userDB[0]?.steamID)
+				res.error =
+					"The user you mentioned did not register a steamID in the database.";
+			else res.success = true;
+			res.data = {
+				type: "steamID",
+				value: userDB[0].steamID,
+			};
+			break;
+		}
+		case "name": {
+			const player = await getPlayer(input!);
+			console.log(player);
+			if (!player.success) res.error = player.error;
+			else {
+				res.success = true;
+				res.data.value = player.data.steam_id;
+			}
+			break;
+		}
+		case "steamID":
+			break;
+	}
+	return res;
+}
