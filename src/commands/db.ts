6fa6d37887650965824c17a56079a54e4618feac
AlphaKeks <contact@schnose.eu>
2022-08-08 17:20:16 +0200

    1.18.0: added /db

---
+++ src/commands/db.ts
@@ -0,0 +1,40 @@
+import {
+	SlashCommandBuilder,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+} from "discord.js";
+import userSchema from "../lib/schemas/user";
+import { reply } from "../lib/functions/discord";
+import "dotenv/config";
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("db")
+		.setDescription("Check your current database entries."),
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		const userDB = await userSchema.find({ discordID: interaction.user.id });
+		if (!userDB[0])
+			return reply(interaction, {
+				content: "You don't have any database entries yet.",
+			});
+
+		let [userID, steamID, mode]: string[] = [
+			userDB[0].discordID!,
+			userDB[0].steamID || "none",
+			userDB[0].mode || "none",
+		];
+
+		const embed = new EmbedBuilder()
+			.setColor([116, 128, 194])
+			.setTitle("Your current database entries:")
+			.setDescription(
+				`> userID: ${userID}
+		> steamID: ${steamID}
+		> mode: ${mode}`
+			)
+			.setFooter({ text: "(͡ ͡° ͜ つ ͡͡°)7", iconURL: process.env.ICON });
+
+		return reply(interaction, { embeds: [embed] });
+	},
+};
