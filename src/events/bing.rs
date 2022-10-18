a8c558348dea76233af7d9a64de6cc4269536c23
AlphaKeks <alphakeks@schnose.eu>
2022-10-18 20:37:07 +0200

    0.1.9: even cleaner rng

--- src/events/bing.rs
+++ src/events/bing.rs
@@ -3,15 +3,14 @@ use serenity::{model::channel::Message, prelude::Context};
 
 pub async fn message(ctx: Context, message: Message) {
 	if message.content.to_lowercase().starts_with("bing?") {
-		let chilling: u8 = if message.author.id == 291585142164815873 {
-			1
-		} else if message.author.id == 295966419261063168 {
-			0
-		} else {
-			rand::thread_rng().gen_range(0..=1)
-		};
+		let chilling: u8 = rand::thread_rng().gen_range(0..=1);
 
-		let chilling = if chilling == 1 {
+		let chilling = if message.author.id == 241247299769073665 {
+			"<:whatsapp:998940776136450128>"
+		} else if message.author.id == 291585142164815873 // me
+			|| message.author.id == 295966419261063168 // iBrahizy
+			|| chilling == 1
+		{
 			"chilling 🥶"
 		} else {
 			"no <:joePensive:975446358796410890>"
