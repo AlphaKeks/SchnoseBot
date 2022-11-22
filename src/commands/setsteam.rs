820602f570b6a20bc5c64de90e701a8badef462d
AlphaKeks <alphakeks@dawn.sh>
2022-11-22 21:35:40 +0100

    0.4.6: fixed /setsteam

--- src/commands/setsteam.rs
+++ src/commands/setsteam.rs
@@ -30,58 +30,66 @@ pub async fn execute(mut ctx: InteractionData<'_>) -> Result<()> {
 		return ctx.reply(Message("Please enter a valid SteamID.")).await;
 	}
 
-	// try to update db entry
-	match ctx
-		.db
-		.find_one_and_update(
-			doc! { "discordID": ctx.user.id.to_string() },
-			doc! { "$set": { "steamID": &steam_id } },
-			None,
-		)
-		.await
-	{
-		Ok(_) => {
-			return ctx
-				.reply(Message(&format!(
-					"Successfully updated SteamID for <@{}>. New SteamID: `{}`",
-					ctx.user.id.as_u64(),
-					steam_id,
-				)))
-				.await
-		},
-		// try to create new entry
-		Err(_) => match ctx
-			.db
-			.insert_one(
-				UserSchema {
-					name: ctx.user.name.clone(),
-					discordID: ctx.user.id.to_string(),
-					steamID: Some(steam_id.clone()),
-					mode: None,
-				},
-				None,
-			)
-			.await
-		{
-			Ok(_) => {
-				return ctx
-					.reply(Message(&format!(
-						"Successfully set SteamID `{}` for <@{}>.",
-						steam_id,
-						ctx.user.id.as_u64()
-					)))
+	match ctx.db.find_one(doc! { "discordID": ctx.user.id.to_string() }, None).await {
+		Ok(document) => match document {
+			// update document
+			Some(_entry) => {
+				match ctx
+					.db
+					.find_one_and_update(
+						doc! { "discordID": ctx.user.id.to_string() },
+						doc! { "$set": { "steamID": &steam_id } },
+						None,
+					)
 					.await
+				{
+					Ok(_) => {
+						return ctx
+							.reply(Message(&format!(
+								"Successfully updated SteamID for <@{}>. New SteamID: {}",
+								ctx.user.id.as_u64(),
+								steam_id,
+							)))
+							.await
+					},
+					Err(why) => {
+						log::error!("[{}]: {} => {:?}", file!(), line!(), why);
+						return ctx.reply(Message("Failed to update databse.")).await;
+					},
+				}
 			},
-			Err(why) => {
-				log::error!(
-					"[{}]: {} => {}\n{:?}",
-					file!(),
-					line!(),
-					"Failed to insert into database.",
-					why
-				);
-				return ctx.reply(Message("Failed to insert into database.")).await;
+			// create new document
+			None => match ctx
+				.db
+				.insert_one(
+					UserSchema {
+						name: ctx.user.name.clone(),
+						discordID: ctx.user.id.to_string(),
+						steamID: Some(steam_id.clone()),
+						mode: None,
+					},
+					None,
+				)
+				.await
+			{
+				Ok(_) => {
+					return ctx
+						.reply(Message(&format!(
+							"Successfully set SteamID `{}` for <@{}>.",
+							steam_id,
+							ctx.user.id.as_u64()
+						)))
+						.await
+				},
+				Err(why) => {
+					log::error!("[{}]: {} => {:?}", file!(), line!(), why);
+					return ctx.reply(Message("Failed to create database entry.")).await;
+				},
 			},
+		},
+		Err(why) => {
+			log::error!("[{}]: {} => {}\n{:?}", file!(), line!(), "Failed to acces database.", why);
+			return ctx.reply(Message("Failed to access database.")).await;
 		},
 	}
 }
