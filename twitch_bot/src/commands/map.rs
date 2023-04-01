51d36e24153be41dbb242d4983139b387cafdd2c
AlphaKeks <alphakeks@dawn.sh>
2023-04-01 22:04:07 +0200

    fixup! fetch non global maps on twitch bot

--- twitch_bot/src/commands/map.rs
+++ twitch_bot/src/commands/map.rs
@@ -17,12 +17,17 @@ pub async fn execute(map: GlobalMap) -> Result<String> {
 		mapper_name,
 		mapper_steam_id: _,
 		filesize: _,
+		validated,
 		created_on: _,
 		updated_on,
 		url: _,
 		thumbnail: _,
 	} = map;
 
+	let global = match validated {
+		true => "",
+		false => "[NON-GLOBAL] ",
+	};
 	let tier = tier as u8;
 	let bonuses = courses.len() - 1;
 	let plural = if bonuses == 1 { "" } else { "es" };
@@ -30,6 +35,6 @@ pub async fn execute(map: GlobalMap) -> Result<String> {
 	sleep(Duration::from_millis(727)).await;
 
 	Ok(format!(
-		"{name} (T{tier}) - {bonuses} Bonus{plural} - Made by {mapper_name} - Last Updated on {updated_on}"
+		"{global}{name} (T{tier}) - {bonuses} Bonus{plural} - Made by {mapper_name} - Last Updated on {updated_on}"
 	))
 }
